72dde3281de68458925844d64babfc24171ca5bb
nemethricsi01 <54413738+nemethricsi01@users.noreply.github.com>
2020-01-29 18:31:06 +0100

    Add files via upload

--- newmain.c
+++ newmain.c
@@ -53,8 +53,8 @@
 #include <xc.h>
 #define _XTAL_FREQ 32000000
 
-unsigned int levels[6] = {819,938,1229,1638,2048,2457};//1v, 1,5v, 2v, 2,5v, 3v
-unsigned int times[6] = {30,60,90,120,150,180};
+unsigned int levels[8] = {1360,940,670,530,430,360,305,250};//-25,-30,-35,-40,-45,-50,-55,-60
+unsigned int times[8] = {6,60,90,120,150,180,210,240};
 
 void adc_init(void);
 int adc_read(void);
@@ -66,11 +66,14 @@ int reading;
 int jumpers;
 int offtimer;
 int ontimer;
-int powerontime = 1500;
 
 void main(void) {
-    TRISCbits.TRISC3 = 0;//active pwr led
-    TRISCbits.TRISC2 = 0;//alarm led
+    TRISBbits.TRISB4 = 0;//alarm relay
+    TRISBbits.TRISB6 = 0;//alarm led
+    TRISBbits.TRISB5 = 0;//underlevel led
+    TRISC = 0b11111111;
+    ANSELC = 0b00000000;
+    WPUC = 0b11111111;//weak pullup on c port
     OSCCON1bits.NOSC = 0b110;//intosc,32mhz
     OSCCON1bits.NDIV = 0;//divide the osc output by 1
     OSCFRQbits.HFFRQ = 0b110;//intosc freq setting:32mhz
@@ -83,26 +86,23 @@ void main(void) {
     
     pwm_init();
     adc_init();
-    pwm_set(600);
     
     while(1)
     {
-       pwm_set( (adc_read()>>2) );
-       __delay_ms(100);
-       
-      
+       pwm_set( (adc_read()>>2) );//level led pwm  
        if(millisec >= 1000)
        {
            reading = adc_read();
-           //jumpers = PORTC;
+           jumpers = ~PORTC;
            if(reading <levels[(jumpers&0b111)])
            {
               offtimer++; 
               ontimer = 0;
+              LATBbits.LATB5 = 1;
               if (offtimer >=times[((jumpers&0b11100000)>>5)])
            {
-               LATCbits.LATC2 = 1;
-               
+               LATBbits.LATB4 = 1;
+               LATBbits.LATB6 = 1;
                offtimer = times[((jumpers&0b11100000)>>5)];
            }
            }
@@ -111,21 +111,16 @@ void main(void) {
            {
               ontimer++; 
               offtimer = 0;
-           if (ontimer >=10)
+              LATBbits.LATB5 = 0;
+           if (ontimer >=3)
            {
-               LATCbits.LATC2 = 0;
-               ontimer = 10;
+               LATBbits.LATB4 = 0;
+               LATBbits.LATB6 = 0;
+               ontimer = 3;
                
            }
            }
-           
-           
-           
-           
-           
-           
-           
-           
+
            millisec = 0;
        }
        
@@ -175,32 +170,7 @@ void pwm_set(unsigned int value)
 void __interrupt () my_isr_routine (void) 
 {
 if(TMR0IE&&TMR0IF)
-{
-    if(powerontime == 1500)
-    {
-        LATCbits.LATC3 = 1;
-    }
-    if(powerontime == 1125)
-    {
-        LATCbits.LATC3 = 0;
-    }
-    if(powerontime == 750)
-    {
-        LATCbits.LATC3 = 1;
-    }
-    if(powerontime == 375)
-    {
-        LATCbits.LATC3 = 0;
-    }
-    if(powerontime == 0)
-    {
-        LATCbits.LATC3 = 1;
-    }
-    if(powerontime >1)
-    {
-        --powerontime;
-    }
-    
+{ 
     TMR0L = 6;
     millisec++;
     TMR0IF = 0;
